7484ffef365c3e6b1f2061ccb2585aee77fa1215
Андрей <okdahorosho@gmail.com>
2016-10-06 21:58:44 +0300

    Add files via upload

---
+++ Zadacha1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+    double y;
+    int x;
+    cout << "Введите x:";
+    cin >> x;
+    if (x==10) cout << "Данный значение не входит в ОДЗ!";
+    else {
+        y = ((pow(x,5) - (7*pow(x,3)) - 3) / (x-10));
+        cout << "Полученное значение y:";
+        cout << y;
+    }
+}
