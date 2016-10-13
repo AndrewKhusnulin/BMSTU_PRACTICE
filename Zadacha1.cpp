0208257f6c2e649b7cb316fd3c70447ff42914a7
Андрей <okdahorosho@gmail.com>
2016-10-13 18:19:34 +0300

    Update Zadacha1.cpp

--- Zadacha1.cpp
+++ Zadacha1.cpp
@@ -1,16 +1,16 @@
-#include <iostream>
+ #include <iostream>
 #include <math.h>
 using namespace std;
 int main()
 {
     double y; 
     int x;
-    cout << "Введите x:";
+    cout << "Введите x:" << endl;
     cin >> x;
-    if (x==10) cout << "Данный значение не входит в ОДЗ!";
+    if (x==10) cout << "Данный значение не входит в ОДЗ!" << endl;
     else {
         y = ((pow(x,5) - (7*pow(x,3)) - 3) / (x-10));
-        cout << "Полученное значение y:";
-        cout << y;
+        cout << "Полученное значение y:" << endl;
+        cout << y << endl;
     }
 }
