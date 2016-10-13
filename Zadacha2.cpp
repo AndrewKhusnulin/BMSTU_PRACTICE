0351c0ce92f25e15be8f4f0585059a1453e5319a
Андрей <okdahorosho@gmail.com>
2016-10-13 18:18:27 +0300

    Update Zadacha2.cpp

--- Zadacha2.cpp
+++ Zadacha2.cpp
@@ -3,47 +3,47 @@
 using namespace std;
 int main() {
     int x;
-    cout << "Введите номер месяца:";
+    cout << "Введите номер месяца:" << endl;
     cin >> x;
     switch (x) {
-        case 1:
-            cout << "Январь";
-            break;
-        case 2:
-            cout << "Февраль";
-            break;
-        case 3:
-            cout << "Март";
-            break;
-        case 4:
-            cout << "Апрель";
-            break;
-        case 5:
-            cout << "Май";
-            break;
-        case 6:
-            cout << "Июнь";
-            break;
-        case 7:
-            cout << "Июль";
-            break;
-        case 8:
-            cout << "Август";
-            break;
-        case 9:
-            cout << "Сентябрь";
-            break;
-        case 10:
-            cout << "Октябрь";
-            break;
-        case 11:
-            cout << "Ноябрь";
-            break;
-        case 12:
+        case 1:{
+            cout << "Январь" << endl;
+            break;}
+        case 2:{
+            cout << "Февраль" << endl;
+            break;}
+        case 3:{
+            cout << "Март" << endl;
+            break;}
+        case 4:{
+            cout << "Апрель" << endl;
+            break;}
+        case 5:{
+            cout << "Май" << endl;
+            break;}
+        case 6:{
+            cout << "Июнь" << endl;
+            break;}
+        case 7:{
+            cout << "Июль" << endl;
+            break;}
+        case 8:{
+            cout << "Август" << endl;
+            break;}
+        case 9:{
+            cout << "Сентябрь" << endl;
+            break;}
+        case 10:{
+            cout << "Октябрь" << endl;
+            break;}
+        case 11:{
+            cout << "Ноябрь" << endl;
+            break;}
+        case 12:{
             cout << "Декабрь" << endl;
-            break;
+            break;}
         default:
-            cout << "Такого месяца нет";
+            cout << "Такого месяца нет" << endl;
     }
     return 0;
 }
